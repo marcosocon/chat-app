74a09f24d6be34835c128744f81c24f9dc4f538a
marcosocon <oconmarcos97@gmail.com>
2016-06-10 14:12:59 -0300

    verifying user

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000, function(){
-	console.log('Express server running on port 3000');
+	console.log('Express server running on port :3000');
 });
