436ce67054d65bf23efcfebeb7f90fe2e7d436b7
marcosocon <oconmarcos97@gmail.com>
2016-06-20 20:27:08 -0300

    added room data inside json and requiring it

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
+var rooms = require('./data/rooms.json');
 
-app.use(express.static("public"));
-app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
+app.set('view engine', 'jade');
 
 
 app.get('/', function(req, res){
-	res.render('index.jade',{ title: "Home" });
+	res.render('index',{ title: 'Home' });
 });
 
 app.get('/rooms', function(req, res){
-	res.render('room.jade',{ title: "Rooms" });
+	res.render('room',{ title: 'Rooms', rooms:rooms });
 });
 
 app.listen(3000, function(){
